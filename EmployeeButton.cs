6af2dee469b18a71d59e5ee43cb8e7de82410264
Lelaus-Ivan <53258091+Lelaus-Ivan@users.noreply.github.com>
2019-11-07 20:21:55 +0500

    chapter 25
    buttons with characteristic

---
+++ EmployeeButton.cs
@@ -0,0 +1,10 @@
+namespace Petzold.ContentTemplateDemo
+{
+    public partial class EmployeeButton
+    {
+        public EmployeeButton()
+        {
+            InitializeComponent();
+        }
+    }
+}
